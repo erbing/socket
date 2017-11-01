8bb566c176e0a905fc13f8db2407323d6155fec3
zigzga <447587073@qq.com>
2017-11-02 00:33:44 +0800

    open socket

--- app.js
+++ app.js
@@ -10,16 +10,25 @@ app.use(express.static(path.join(__dirname, 'src')));
 app.set('views', path.join(__dirname, 'service'));
 app.set('view engine', 'ejs');
 
+// 创建 socket 服务
 var IO = socket(server)
-
-
 // 定义用户群组
 var roomInfo = {}
+
+IO.on('connection', function(socket){
+    var url = socket.request.headers.referer
+    console.log(url)
+    var roomId = url.split('/')[url.split('/').length - 1]
+    var user = ''
+    socket.on('join', function(){
+        socket.join(roomId)
+    })
+})
 
 
 router.get('/index/:roomId' ,function(req, res){
     var roomId = req.params.roomId
-
+    console.log(req.params)
     // 渲染页面
     res.render('index', {
         roomId: roomId,
