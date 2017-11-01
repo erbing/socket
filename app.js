d315cec02c2b3896554f41f584a65a2957d5622e
zigzga <447587073@qq.com>
2017-11-01 23:59:44 +0800

    get server

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
-import Vue from 'vue'
+const express = require('express')
+const path = require('path')
+const socket = require('socket.io')
 
-new Vue({
-    data: {
-        a: 123
-    }
+var router = express.Router()
+
+var app = express()
+var server = require('http').Server(app)
+app.use(express.static(path.join(__dirname, 'src')));
+app.set('views', path.join(__dirname, 'service'));
+app.set('view engine', 'ejs');
+
+var IO = socket(server)
+
+
+// 定义用户群组
+var roomInfo = {}
+
+
+router.get('/index/:roomId' ,function(req, res){
+    var roomId = req.params.roomId
+
+    // 渲染页面
+    res.render('index', {
+        roomId: roomId,
+        users: roomInfo[roomId]
+    })
 })
+
+app.use('/', router)
+
+server.listen(3000, function(){
+    console.log('erver listening on port 3000')
+}) 
